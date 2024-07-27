f669be310e5aa52fb32cfd232c5a3cc1423cd2ac
Ayush H <jqghebri@gmail.com>
2024-07-28 02:08:51 +0530

    creation

--- src/index.mjs
+++ src/index.mjs
@@ -1,9 +1,11 @@
 import express from 'express';
 import routes from './routes/index.mjs';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
 app.use(express.json())
+app.use(cookieParser());
 app.use(routes);
 
 const PORT = process.env.PORT || 3000;
@@ -13,5 +15,6 @@ app.listen(PORT, () => {
 });
 
 app.get("/", (request, response) => {
+    response.cookie("hello","world", { maxAge: 60000 * 60 * 2 });
     response.status(201).send({msg: "Hello, World!"});
 });
--- src/routes/products.mjs
+++ src/routes/products.mjs
@@ -3,6 +3,8 @@ import{ Router } from 'express';
 const router = Router();
 
 router.get("/api/products", (request, response) => {
+    console.log(request.headers.cookie);
+    console.log(request.cookies);
     response.send([{ id: 123, name: "Laptop", price: 1000 }]); 
 });
 
