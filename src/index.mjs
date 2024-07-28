046879eb2ea13f51ec34897a8007079018c3993d
Ayush H <jqghebri@gmail.com>
2024-07-28 16:39:55 +0530

    creation

--- src/index.mjs
+++ src/index.mjs
@@ -1,52 +1,73 @@
-import express from 'express';
-import routes from './routes/index.mjs';
-import cookieParser from 'cookie-parser';
-import session from 'express-session';
-import { mockUsers } from './utils/constants.mjs';
+import express from "express";
+import routes from "./routes/index.mjs";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import { mockUsers } from "./utils/constants.mjs";
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 app.use(cookieParser("helloworld"));
 app.use(
-    session({
-        secret: "ayush the dev",
-        saveUninitialized: false,
-        resave: false,
-        cookie: { 
-            maxAge: 60000 * 60,
-        }
-    })
+  session({
+    secret: "ayush the dev",
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: 60000 * 60,
+    },
+  })
 );
 app.use(routes);
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log('Running on Port ${PORT}');
+  console.log("Running on Port ${PORT}");
 });
 
 app.get("/", (request, response) => {
-    console.log(request.session);
-    console.log(request.session.id);
-    request.session.visited = true;
-    response.cookie("hello","world", { maxAge: 30000, signed: true });
-    response.status(201).send({msg: "Hello, World!"});
+  console.log(request.session);
+  console.log(request.session.id);
+  request.session.visited = true;
+  response.cookie("hello", "world", { maxAge: 30000, signed: true });
+  response.status(201).send({ msg: "Hello, World!" });
 });
 
-app.post('/api/auth', (request, response) => {
-    const { 
-        body: { username, password },
-    } = request;
-    const findUser = mockUsers.find((user) => user.username === username);
-    if (!findUser || findUser.password !==password) 
-        return response.status(401).send({ msg: "Invalid credentials" });
+app.post("/api/auth", (request, response) => {
+  const {
+    body: { username, password },
+  } = request;
+  const findUser = mockUsers.find((user) => user.username === username);
+  if (!findUser || findUser.password !== password)
+    return response.status(401).send({ msg: "Invalid credentials" });
 
-    request.session.user = findUser;
-    return response.status(200).send(findUser);
+  request.session.user = findUser;
+  return response.status(200).send(findUser);
 });
 
-app.get('/api/auth/status', (request, response) => {
-    return request.session.user ? response.status(200).send(request.session.user): response.status(401).send({ msg: "Unauthorized" });
+app.get("/api/auth/status", (request, response) => {
+  request.sessionStore.get(request.sessionID, (err, session) => {
+    console.log(session);
+  });
+  return request.session.user
+    ? response.status(200).send(request.session.user)
+    : response.status(401).send({ msg: "Unauthorized" });
+});
 
+app.post("/api/cart", (request, response) => {
+  if (!request.session.user) return response.status(401);
+  const { body: item } = request;
+  const { cart } = request.session;
+  if (cart) {
+    cart.push(item);
+  } else {
+    request.session.cart = [item];
+  }
+  return response.status(201).send(item);
+});
+
+app.get("/api/cart", (request, response) => {
+  if (!request.session.user) return response.sendStatus(401);
+  return response.send(request.session.cart ?? []);
 });
