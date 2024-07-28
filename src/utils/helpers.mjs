1d587caea0d9df57b5ec3357d9d45506eb0e270c
Ayush H <jqghebri@gmail.com>
2024-07-29 01:31:22 +0530

    creation

--- src/utils/helpers.mjs
+++ src/utils/helpers.mjs
@@ -8,3 +8,9 @@ export const hashPassword = (password) => {
     console.log(salt);
     return bcrypt.hashSync(password, salt)
 };
+
+
+export const comparePassword = (plain, hashed) => {
+    return bcrypt.compareSync(plain, hashed);
+};
+
