9b031d9133989b9af0f7ab90a9e899821932f297
Ayush H <jqghebri@gmail.com>
2024-07-29 04:23:28 +0530

    creation

--- src/index.mjs
+++ src/index.mjs
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import session from "express-session";
 import passport from "passport";
 import mongoose from "mongoose";
+import MongoStore from "connect-mongo";
 import "./strategies/local-strategy.mjs";
 
 const app = express();
@@ -24,6 +25,9 @@ app.use(
     cookie: {
       maxAge: 60000 * 60,
     },
+    store: MongoStore.create({
+      client: mongoose.connection.getClient(),
+    }),
   })
 );
 
