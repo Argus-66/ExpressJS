2b8338b7c8c45fc7959b784cea8c49bfe5f497b5
Ayush H <jqghebri@gmail.com>
2024-07-28 12:41:14 +0530

    creation

--- src/index.mjs
+++ src/index.mjs
@@ -1,11 +1,22 @@
 import express from 'express';
 import routes from './routes/index.mjs';
 import cookieParser from 'cookie-parser';
+import session from 'express-session';
 
 const app = express();
 
 app.use(express.json())
 app.use(cookieParser("helloworld"));
+app.use(
+    session({
+        secret: "ayush the dev",
+        saveUninitialized: false,
+        resave: false,
+        cookie: { 
+            maxAge: 60000 * 60,
+        }
+    })
+);
 app.use(routes);
 
 const PORT = process.env.PORT || 3000;
@@ -15,6 +26,8 @@ app.listen(PORT, () => {
 });
 
 app.get("/", (request, response) => {
+    console.log(request.session);
+    console.log(request.session.id); //or .sessionID
     response.cookie("hello","world", { maxAge: 30000, signed: true });
     response.status(201).send({msg: "Hello, World!"});
 });
