71ad17006867a56c9ada3778cb33a10cd1da34c9
Ayush H <jqghebri@gmail.com>
2024-07-29 18:00:46 +0530

    creation

--- src/index.mjs
+++ src/index.mjs
@@ -62,6 +62,8 @@ app.get('/api/auth/discord', passport.authenticate('discord'))
 app.get('/api/auth/discord/redirect', 
   passport.authenticate("discord"), 
   (request, response) => {
+    console.log(request.session);
+    console.log(request.user);
     response.sendStatus(200);
 });
 
--- src/strategies/discord-strategy.mjs
+++ src/strategies/discord-strategy.mjs
@@ -1,5 +1,21 @@
 import passport from "passport";
 import { Strategy } from "passport-discord";
+import { DiscordUser } from "../mongoose/schemas/discord-user.mjs";
+
+passport.serializeUser((user, done) => {
+  console.log(`Inside Serializing User`);
+  console.log(user);
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const findUser = await DiscordUser.findById(id);
+    return findUser ? done(null, findUser) : done(null, null);
+  } catch (err) {
+    done(err, null);
+  }
+});
 
 export default passport.use(
   new Strategy(
@@ -9,8 +25,27 @@ export default passport.use(
       callbackURL: "http://localhost:3000/api/auth/discord/redirect",
       scope: ["identify"],
     },
-    (accessToken, refreshToken, profile, done) => {
-      console.log(profile);
+    async (accessToken, refreshToken, profile, done) => {
+      let findUser;
+      try {
+        findUser = await DiscordUser.findOne({ discordId: profile.id });
+      } catch (err) {
+        return done(err, null);
+      }
+      try {
+        if (!findUser) {
+          const newUser = new DiscordUser({
+            username: profile.username,
+            discordId: profile.id,
+          });
+          const newSavedUser = await newUser.save();
+          return done(null, newSavedUser);
+        }
+        return done(null, findUser);
+      } catch (err) {
+        console.log(err);
+        return done(err, null);
+      }
     }
   )
 );
